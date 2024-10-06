5fb8b32927b9b1737a81a19c9d391784a6180f76
Koncz Máté <h265733@stud.u-szeged.hu>
2024-10-07 00:17:50 +0200

    create eventservice with tests

--- src/main/java/com/example/eowa/service/EventService.java
+++ src/main/java/com/example/eowa/service/EventService.java
@@ -1,4 +1,37 @@
 package com.example.eowa.service;
 
+import com.example.eowa.model.Event;
+import com.example.eowa.repository.EventRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class EventService {
+
+    private EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public Event saveEvent(Event event){
+        return eventRepository.save(event);
+    }
+
+    public void deleteEventById(Long id){
+        eventRepository.deleteById(id);
+    }
+
+    public void deleteAllEvent(){
+        eventRepository.deleteAll();
+    }
+
+    public Event getEventById(Long id){
+        return eventRepository.findById(id).orElse(null);
+    }
+
+    public Event updateEvent(Event event){
+        return eventRepository.save(event);
+    }
 }
---
+++ src/test/java/com/example/eowa/service/EventServiceTests.java
@@ -0,0 +1,77 @@
+package com.example.eowa.service;
+
+import com.example.eowa.model.Event;
+import com.example.eowa.model.User;
+import exceptions.userExceptions.UserException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.CustomAutowireConfigurer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class EventServiceTests {
+
+    @Autowired
+    private SessionService sessionService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private EventService eventService;
+
+    @BeforeEach
+    public void beforeTests(){
+        sessionService.deleteAllSession();
+        eventService.deleteAllEvent();
+        userService.deleteAllUsers();
+    }
+
+    @Test
+    public void shouldSaveEvent() throws UserException {
+        User user = new User("felh","asznalo1","email@gmail.com");
+        User savedUser = userService.saveUser(user);
+        Event event = new Event(savedUser,"kertiparti",new HashSet<User>());
+        Event savedEvent = eventService.saveEvent(event);
+        Assertions.assertNotNull(savedEvent);
+    }
+
+    @Test
+    public void shouldGetEventById() throws UserException {
+        User user = new User("felh","asznalo1","email@gmail.com");
+        User savedUser = userService.saveUser(user);
+        Event event = new Event(savedUser,"kertiparti",new HashSet<User>());
+        Long storedId = eventService.saveEvent(event).getId();
+
+        Assertions.assertEquals("kertiparti",eventService.getEventById(storedId).getEventName());
+    }
+
+    @Test
+    public void shouldSaveParticipantsOnEvent() throws UserException {
+        User user = new User("felh","asznalo1","email@gmail.com");
+        User savedUser = userService.saveUser(user);
+
+        User p1 = new User("feri","asznalo1","feri@gmail.com");
+        User savedp1 = userService.saveUser(p1);
+
+        User p2 = new User("dani","asznalo1","dani@gmail.com");
+        User savedp2 = userService.saveUser(p2);
+
+        Set<User> participants = new HashSet<>();
+        participants.add(savedp1);
+        participants.add(savedp2);
+
+        Event event = new Event(savedUser,"kertiparti",participants);
+        Event savedEvent = eventService.saveEvent(event);
+
+        Assertions.assertEquals(savedEvent.getParticipants().size(),2);
+    }
+}
