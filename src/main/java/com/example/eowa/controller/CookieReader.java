f010922f93c15127fd54514cb3dcfaed8ac5f534
Koncz Máté <h265733@stud.u-szeged.hu>
2024-10-10 14:55:52 +0200

    create custom cookieReader class

---
+++ src/main/java/com/example/eowa/controller/CookieReader.java
@@ -0,0 +1,24 @@
+package com.example.eowa.controller;
+
+import jakarta.servlet.http.Cookie;
+
+public class CookieReader {
+    private Cookie[] cookies;
+
+    public CookieReader(Cookie[] cookies) {
+        this.cookies = cookies;
+    }
+
+    public boolean hasCookie(String cookieName){
+        return getCookie(cookieName) != null;
+    }
+
+    public Cookie getCookie(String cookieName){
+        for (Cookie cookie : cookies) {
+            if (cookieName.equals(cookie.getName())) {
+                return cookie;
+            }
+        }
+        return null;
+    }
+}
