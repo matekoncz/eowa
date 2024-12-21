78b864f8b4fc067b34f5fcbfeb0e54323eb6f21c
Koncz Máté <h265733@stud.u-szeged.hu>
2024-12-21 17:48:32 +0100

    update eventController to include all new features (set unavilable days/hours, set user opinion, join event with invitation code)

--- src/main/java/com/example/eowa/controller/EventController.java
+++ src/main/java/com/example/eowa/controller/EventController.java
@@ -2,8 +2,11 @@ package com.example.eowa.controller;
 
 import com.example.eowa.exceptions.CalendarExceptions.CalendarException;
 import com.example.eowa.exceptions.authenticationExceptions.AuthenticationException;
+import com.example.eowa.exceptions.authenticationExceptions.InvalidInvitationCodeException;
 import com.example.eowa.exceptions.authenticationExceptions.UserIsNotEventOwnerException;
+import com.example.eowa.exceptions.authenticationExceptions.UserIsNotParticipantException;
 import com.example.eowa.model.Event;
+import com.example.eowa.model.Opinion;
 import com.example.eowa.model.User;
 import com.example.eowa.service.AuthService;
 import com.example.eowa.service.EventService;
@@ -35,7 +38,10 @@ public class EventController {
     }
 
     @PostMapping("/create")
-    public HttpServletResponse createEvent(@RequestBody Event event, @CookieValue("jsessionid") String jsessionid, HttpServletResponse response) throws AuthenticationException, IOException {
+    public HttpServletResponse createEvent(
+            @RequestBody Event event,
+            @CookieValue("jsessionid") String jsessionid,
+            HttpServletResponse response) throws AuthenticationException, IOException {
         authService.validateSession(jsessionid);
         User owner = authService.getUserBySessionId(jsessionid);
         event.setOwner(owner);
@@ -46,7 +52,10 @@ public class EventController {
     }
 
     @GetMapping("/{id}")
-    public HttpServletResponse getEventById(@CookieValue("jsessionid") String jsessionid, @PathVariable("id") long eventId, HttpServletResponse response) throws IOException, AuthenticationException {
+    public HttpServletResponse getEventById(
+            @CookieValue("jsessionid") String jsessionid,
+            @PathVariable("id") long eventId,
+            HttpServletResponse response) throws IOException, AuthenticationException {
         authService.validateSession(jsessionid);
         authService.validateParticipant(jsessionid,eventId);
         Event event = eventService.getEventById(eventId);
@@ -56,7 +65,9 @@ public class EventController {
     }
 
     @GetMapping("/currentuser-events")
-    public HttpServletResponse getCurrentUserEvents(@CookieValue("jsessionid") String jsessionid, HttpServletResponse response) throws AuthenticationException, IOException {
+    public HttpServletResponse getCurrentUserEvents(
+            @CookieValue("jsessionid") String jsessionid,
+            HttpServletResponse response) throws AuthenticationException, IOException {
         authService.validateSession(jsessionid);
         User currentUser = authService.getUserBySessionId(jsessionid);
         Set<Event> events = currentUser.getEvents();
@@ -65,7 +76,10 @@ public class EventController {
     }
 
     @DeleteMapping("/{id}")
-    public HttpServletResponse deleteEventById(@CookieValue("jsessionid") String jsessionid, @PathVariable("id") long id, HttpServletResponse response) throws AuthenticationException {
+    public HttpServletResponse deleteEventById(
+            @CookieValue("jsessionid") String jsessionid,
+            @PathVariable("id") long id,
+            HttpServletResponse response) throws AuthenticationException {
         authService.validateSession(jsessionid);
         authService.validateEventOwner(jsessionid, id);
         eventService.deleteEventById(id);
@@ -75,7 +89,11 @@ public class EventController {
 
     @Transactional
     @PutMapping("/{id}/add-users")
-    public HttpServletResponse addParticipants(@CookieValue("jsessionid") String jsessionid, @PathVariable("id") long id, @RequestBody Set<User> participants, HttpServletResponse response) throws AuthenticationException {
+    public HttpServletResponse addParticipants(
+            @CookieValue("jsessionid") String jsessionid,
+            @PathVariable("id") long id,
+            @RequestBody Set<User> participants,
+            HttpServletResponse response) throws AuthenticationException {
         authService.validateSession(jsessionid);
         authService.validateEventOwner(jsessionid, id);
         eventService.getEventById(id).addALlParticipant(participants);
@@ -84,11 +102,92 @@ public class EventController {
     }
 
     @PutMapping("/{id}/add-calendar")
-    public HttpServletResponse addCalendar(@CookieValue("jsessionid") String jsessionid,  @PathVariable("id") long id, @RequestParam("start") String startString, @RequestParam("end") String endString,@RequestParam("zone") String zoneId, HttpServletResponse response) throws UserIsNotEventOwnerException, CalendarException {
+    public HttpServletResponse addCalendar(
+            @CookieValue("jsessionid") String jsessionid,
+            @PathVariable("id") long id,
+            @RequestParam("start") String startString,
+            @RequestParam("end") String endString,
+            @RequestParam("zone") String zoneId,
+            HttpServletResponse response) throws UserIsNotEventOwnerException, CalendarException {
         authService.validateEventOwner(jsessionid,id);
         LocalDateTime startTime = LocalDateTime.parse(startString);
         LocalDateTime endTime = LocalDateTime.parse(endString);
         eventService.setEventCalendar(id,zoneId,startTime,endTime);
+        response.setStatus(HttpStatus.OK.value());
+        return response;
+    }
+
+    @PutMapping("/{id}/set-unavailable-days")
+    public HttpServletResponse setUnavailableDays(
+            @CookieValue("jsessionid") String jsessionid,
+            @PathVariable("id") long id,
+            @RequestBody Set<Integer> serialNumbers,
+            HttpServletResponse response) throws UserIsNotEventOwnerException {
+        authService.validateEventOwner(jsessionid,id);
+        eventService.setUnavailableDays(id,serialNumbers);
+        response.setStatus(HttpStatus.OK.value());
+        return response;
+    }
+
+    @PutMapping("/{id}/set-unavailable-hours")
+    public HttpServletResponse setUnavailableHours(
+            @CookieValue("jsessionid") String jsessionid,
+            @PathVariable("id") long id,
+            @RequestBody Set<Integer> serialNumbers,
+            HttpServletResponse response) throws UserIsNotEventOwnerException {
+        authService.validateEventOwner(jsessionid,id);
+        eventService.setUnavailableHours(id,serialNumbers);
+        response.setStatus(HttpStatus.OK.value());
+        return response;
+    }
+
+    @PutMapping("/{id}/set-unavailable-hours-periodically")
+    public HttpServletResponse setUnavailableHoursPeriodically(
+            @CookieValue("jsessionid") String jsessionid,
+            @PathVariable("id") long id,
+            @RequestParam("period") int period,
+            @RequestBody Set<Integer> hourNumbers,
+            HttpServletResponse response) throws UserIsNotEventOwnerException {
+        authService.validateEventOwner(jsessionid,id);
+        eventService.setUnavailableHoursPeriodically(id,hourNumbers,period);
+        response.setStatus(HttpStatus.OK.value());
+        return response;
+    }
+
+    @PutMapping("/{id}/set-user-opinion")
+    public HttpServletResponse setUserOpinion(
+            @CookieValue("jsessionid") String jsessionid,
+            @PathVariable("id") long id,
+            @RequestParam("opinion") Opinion.UserOpinion userOpinion,
+            @RequestBody Set<Integer> hourSerials,
+            HttpServletResponse response) throws UserIsNotParticipantException {
+        authService.validateParticipant(jsessionid,id);
+        User user = authService.getUserBySessionId(jsessionid);
+        eventService.setUserOpinion(id,hourSerials,user,userOpinion);
+        response.setStatus(HttpStatus.OK.value());
+        return response;
+    }
+
+    @PutMapping("/{id}/remove-user-opinion")
+    public HttpServletResponse removeUserOpinion(
+            @CookieValue("jsessionid") String jsessionid,
+            @PathVariable("id") long id,
+            @RequestBody Set<Integer> hourSerials,
+            HttpServletResponse response) throws UserIsNotParticipantException {
+        authService.validateParticipant(jsessionid,id);
+        User user = authService.getUserBySessionId(jsessionid);
+        eventService.removeUserOpinion(id,hourSerials,user);
+        response.setStatus(HttpStatus.OK.value());
+        return response;
+    }
+
+    @PutMapping("/join-event")
+    public HttpServletResponse joinEventWithInvitationCode(
+            @CookieValue("jsessionid") String jsessionid,
+            @RequestParam("invitation") String invitation,
+            HttpServletResponse response) throws InvalidInvitationCodeException {
+        User user = authService.getUserBySessionId(jsessionid);
+        eventService.joinEventWithInvitationCode(user,invitation);
         response.setStatus(HttpStatus.OK.value());
         return response;
     }
--- src/test/java/com/example/eowa/controller/EventControllerTests.java
+++ src/test/java/com/example/eowa/controller/EventControllerTests.java
@@ -1,13 +1,8 @@
 package com.example.eowa.controller;
 
 import com.example.eowa.EowaApplication;
-import com.example.eowa.model.Credentials;
-import com.example.eowa.model.Event;
-import com.example.eowa.model.User;
-import com.example.eowa.service.AuthService;
-import com.example.eowa.service.EventService;
-import com.example.eowa.service.SessionService;
-import com.example.eowa.service.UserService;
+import com.example.eowa.model.*;
+import com.example.eowa.service.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.Assertions;
@@ -20,7 +15,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -43,6 +41,9 @@ public class EventControllerTests {
 
     @Autowired
     SessionService sessionService;
+
+    @Autowired
+    CalendarService calendarService;
 
     @Autowired
     MockMvc mockMvc;
@@ -214,4 +215,202 @@ public class EventControllerTests {
 
         Assertions.assertEquals(3,savedEvent.getCalendar().getDays().size());
     }
+
+    @Test
+    public void shouldSetUnavailableDays() throws Exception {
+        User savedUser = userService.saveUser(new User("felh","asznalo1","email@gmail.com"));
+        Credentials credentials = new Credentials();
+        credentials.setPassword("asznalo1");
+        credentials.setUsername("felh");
+
+        String jsessionid = authService.login(credentials);
+
+        Event event = new Event(savedUser,"buli",new HashSet<>(),"");
+        long id = eventService.saveEvent(event).getId();
+
+        eventService.setEventCalendar(id,"CET", LocalDateTime.now(),LocalDateTime.now().plusDays(3));
+
+        String daySerials = objectMapper.writeValueAsString(Set.of(0,1));
+
+        mockMvc.perform(put("/events/"+id+"/set-unavailable-days")
+                        .contentType("application/json")
+                        .accept("application/json")
+                        .content(daySerials)
+                        .cookie(new Cookie("jsessionid",jsessionid)))
+                .andReturn();
+
+        List<Day> updatedDays = eventService.getEventById(id).getCalendar().getDays();
+
+        Assertions.assertFalse(updatedDays.getFirst().isEnabled());
+        Assertions.assertFalse(updatedDays.get(1).isEnabled());
+        Assertions.assertTrue(updatedDays.get(2).isEnabled());
+    }
+
+    @Test
+    public void shouldSetUnavailableHours() throws Exception {
+        User savedUser = userService.saveUser(new User("felh","asznalo1","email@gmail.com"));
+        Credentials credentials = new Credentials();
+        credentials.setPassword("asznalo1");
+        credentials.setUsername("felh");
+
+        String jsessionid = authService.login(credentials);
+
+        Event event = new Event(savedUser,"buli",new HashSet<>(),"");
+        long id = eventService.saveEvent(event).getId();
+
+        eventService.setEventCalendar(id,"CET", LocalDateTime.now(),LocalDateTime.now().plusDays(3));
+
+        String hourSerials = objectMapper.writeValueAsString(Set.of(0,1));
+
+        mockMvc.perform(put("/events/"+id+"/set-unavailable-hours")
+                        .contentType("application/json")
+                        .accept("application/json")
+                        .content(hourSerials)
+                        .cookie(new Cookie("jsessionid",jsessionid)))
+                .andReturn();
+
+        List<Hour> updatedHoursOfFirstDay = eventService.getEventById(id).getCalendar().getDays().getFirst().getHours();
+
+        Assertions.assertFalse(updatedHoursOfFirstDay.getFirst().isEnabled());
+        Assertions.assertFalse(updatedHoursOfFirstDay.get(1).isEnabled());
+        Assertions.assertTrue(updatedHoursOfFirstDay.get(2).isEnabled());
+    }
+
+    @Test
+    public void setUnavailableHoursPeriodically() throws Exception {
+        User savedUser = userService.saveUser(new User("felh","asznalo1","email@gmail.com"));
+        Credentials credentials = new Credentials();
+        credentials.setPassword("asznalo1");
+        credentials.setUsername("felh");
+
+        String jsessionid = authService.login(credentials);
+
+        Event event = new Event(savedUser,"buli",new HashSet<>(),"");
+        long id = eventService.saveEvent(event).getId();
+
+        eventService.setEventCalendar(id,"CET", LocalDateTime.now(),LocalDateTime.now().plusDays(10));
+
+        String hourSerials = objectMapper.writeValueAsString(Set.of(15,16));
+
+        mockMvc.perform(put("/events/"+id+"/set-unavailable-hours-periodically")
+                        .contentType("application/json")
+                        .accept("application/json")
+                        .content(hourSerials)
+                        .queryParam("period","7")
+                        .cookie(new Cookie("jsessionid",jsessionid)))
+                .andReturn();
+
+        List<Day> updatedDays = eventService.getEventById(id).getCalendar().getDays();
+
+        Assertions.assertTrue(updatedDays.getFirst().getHours().getFirst().isEnabled());
+        Assertions.assertFalse(updatedDays.getFirst().getHours().get(15).isEnabled());
+
+        Assertions.assertTrue(updatedDays.get(3).getHours().getFirst().isEnabled());
+        Assertions.assertTrue(updatedDays.get(3).getHours().get(15).isEnabled());
+
+        Assertions.assertTrue(updatedDays.get(7).getHours().getFirst().isEnabled());
+        Assertions.assertFalse(updatedDays.get(7).getHours().get(15).isEnabled());
+    }
+
+
+    @Test
+    public void shouldSetUserOpinion() throws Exception {
+        User savedUser = userService.saveUser(new User("felh","asznalo1","email@gmail.com"));
+        Credentials credentials = new Credentials();
+        credentials.setPassword("asznalo1");
+        credentials.setUsername("felh");
+
+        String jsessionid = authService.login(credentials);
+
+        Event event = new Event(savedUser,"buli",new HashSet<>(),"");
+        long id = eventService.saveEvent(event).getId();
+
+        eventService.setEventCalendar(id,"CET", LocalDateTime.now(),LocalDateTime.now().plusDays(3));
+
+        String hourSerials = objectMapper.writeValueAsString(Set.of(0,1));
+
+        mockMvc.perform(put("/events/"+id+"/set-user-opinion")
+                        .contentType("application/json")
+                        .accept("application/json")
+                        .queryParam("opinion","GOOD")
+                        .content(hourSerials)
+                        .cookie(new Cookie("jsessionid",jsessionid)))
+                .andReturn();
+
+        List<Hour> updatedHoursOfFirstDay = eventService.getEventById(id).getCalendar().getDays().getFirst().getHours();
+
+
+        User currentUser = userService.getUserByUsername(savedUser.getUsername());
+
+        Assertions.assertEquals(calendarService.getUserOpinion(updatedHoursOfFirstDay.getFirst(),currentUser).getUserOpinion(), Opinion.UserOpinion.GOOD);
+        Assertions.assertEquals(calendarService.getUserOpinion(updatedHoursOfFirstDay.get(1),currentUser).getUserOpinion(), Opinion.UserOpinion.GOOD);
+        Assertions.assertEquals(updatedHoursOfFirstDay.get(2).getOpinions().size(),0);
+    }
+
+    @Test
+    public void shouldRemoveUserOpinion() throws Exception {
+        User savedUser = userService.saveUser(new User("felh","asznalo1","email@gmail.com"));
+        Credentials credentials = new Credentials();
+        credentials.setPassword("asznalo1");
+        credentials.setUsername("felh");
+
+        String jsessionid = authService.login(credentials);
+
+        Event event = new Event(savedUser,"buli",new HashSet<>(),"");
+        long id = eventService.saveEvent(event).getId();
+
+        eventService.setEventCalendar(id,"CET", LocalDateTime.now(),LocalDateTime.now().plusDays(3));
+
+        eventService.setUserOpinion(id,Set.of(0,1,2),sessionService.getUserBySessionId(jsessionid), Opinion.UserOpinion.GOOD);
+
+        String hourSerials = objectMapper.writeValueAsString(Set.of(2,1));
+
+        mockMvc.perform(put("/events/"+id+"/remove-user-opinion")
+                        .contentType("application/json")
+                        .accept("application/json")
+                        .content(hourSerials)
+                        .cookie(new Cookie("jsessionid",jsessionid)))
+                .andReturn();
+
+        List<Hour> updatedHoursOfFirstDay = eventService.getEventById(id).getCalendar().getDays().getFirst().getHours();
+
+
+        User currentUser = userService.getUserByUsername(savedUser.getUsername());
+
+        Assertions.assertEquals(calendarService.getUserOpinion(updatedHoursOfFirstDay.getFirst(),currentUser).getUserOpinion(), Opinion.UserOpinion.GOOD);
+        Assertions.assertEquals(updatedHoursOfFirstDay.get(1).getOpinions().size(),0);
+        Assertions.assertEquals(updatedHoursOfFirstDay.get(2).getOpinions().size(),0);
+    }
+
+    @Test
+    public void shouldJoinEVentWIthInvitationCode() throws Exception {
+        User savedUser = userService.saveUser(new User("felh2","asznalo2","email2@gmail.com"));
+        Credentials credentials = new Credentials();
+        credentials.setPassword("asznalo2");
+        credentials.setUsername("felh2");
+
+        String jsessionid = authService.login(credentials);
+
+        Event event = new Event(savedUser,"buli",new HashSet<>(),"");
+        long id = eventService.saveEvent(event).getId();
+
+        eventService.setEventCalendar(id,"CET", LocalDateTime.now(),LocalDateTime.now().plusDays(3));
+
+        User participant = userService.saveUser(new User("felh","asznalo1","email@gmail.com"));
+        Credentials participantCredentials = new Credentials();
+        participantCredentials.setPassword("asznalo1");
+        participantCredentials.setUsername("felh");
+
+        String participantJsessionid = authService.login(participantCredentials);
+
+        mockMvc.perform(put("/events/join-event")
+                        .contentType("application/json")
+                        .accept("application/json")
+                        .queryParam("invitation",eventService.getEventById(id).getInvitationCode())
+                        .cookie(new Cookie("jsessionid",participantJsessionid)))
+                .andReturn();
+
+        authService.validateParticipant(participantJsessionid,id);
+    }
+
 }
