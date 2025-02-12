4c9031c03ee489a14405373a21f97c132aab89f4
Koncz Máté <h265733@stud.u-szeged.hu>
2025-02-12 12:05:25 +0100

    create WebToken class with converter used in endpoints

---
+++ src/main/java/com/example/eowa/service/WebTokenConverter.java
@@ -0,0 +1,20 @@
+package com.example.eowa.service;
+
+import com.example.eowa.model.WebToken;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebTokenConverter implements Converter<String, WebToken> {
+    @Override
+    public WebToken convert(String json) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(json, WebToken.class);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
+}
