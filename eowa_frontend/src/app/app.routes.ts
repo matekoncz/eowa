309f69ec9a90bd8d0f7f10068e5ad86f728df494
Koncz Máté <h265733@stud.u-szeged.hu>
2025-02-19 13:44:10 +0100

    add missing links in app routes

--- eowa_frontend/src/app/app.routes.ts
+++ eowa_frontend/src/app/app.routes.ts
@@ -3,10 +3,18 @@ import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { JoinEventComponent } from './components/join-event/join-event.component';
 import { CreateEventComponent } from './components/create-event/create-event.component';
+import { MyEventsComponent } from './components/my-events/my-events.component';
+import { CreateCalendarComponent } from './components/create-calendar/create-calendar.component';
+import { ViewEventComponent } from './components/event-editor/view-event/view-event.component';
+import { ViewCalendarComponent } from './components/event-editor/view-calendar/view-calendar.component';
 
 export const routes: Routes = [
     {path: 'signup', component: SignupComponent},
     {path: 'login', component: LoginComponent},
     {path: 'join-event', component: JoinEventComponent},
     {path: 'create-event', component: CreateEventComponent},
+    {path: 'my-events', component: MyEventsComponent},
+    {path: 'create-calendar', component: CreateCalendarComponent},
+    {path: 'view-event', component: ViewEventComponent},
+    {path: 'view-calendar', component: ViewCalendarComponent}
 ];
