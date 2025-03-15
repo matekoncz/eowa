5c2aaf117dfa3eec53ee2f7f1172267423da0102
Koncz Máté <h265733@stud.u-szeged.hu>
2025-03-15 15:42:11 +0100

    create repositories for new models

---
+++ src/main/java/com/example/eowa/repository/EventBluePrintRepository.java
@@ -0,0 +1,9 @@
+package com.example.eowa.repository;
+
+import com.example.eowa.model.EventBlueprint;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventBluePrintRepository extends JpaRepository<EventBlueprint,Long> {
+}
