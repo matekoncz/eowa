df564b4b86619a798a9889e7d1b2f263a3a7b4a7
Koncz Máté <h265733@stud.u-szeged.hu>
2025-03-15 15:39:53 +0100

    create model for event blueprints

---
+++ src/main/java/com/example/eowa/model/EventBlueprint.java
@@ -0,0 +1,45 @@
+package com.example.eowa.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class EventBlueprint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String content;
+
+    public EventBlueprint() {
+    }
+
+    public EventBlueprint(String name, String content) {
+        this.name = name;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
