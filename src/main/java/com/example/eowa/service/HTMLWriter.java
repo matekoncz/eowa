ac05d55a9115d13958100ba5dc314d3b83f3007b
Koncz Máté <h265733@stud.u-szeged.hu>
2025-03-19 11:55:56 +0100

    create HTMLWriter class

---
+++ src/main/java/com/example/eowa/service/HTMLWriter.java
@@ -0,0 +1,35 @@
+package com.example.eowa.service;
+
+public class HTMLWriter {
+
+    public static String h1(String content){
+        return evenHtmlElement(content,"h1");
+    }
+
+    public static String h2(String content){
+        return evenHtmlElement(content,"h2");
+    }
+
+    public static String p(String content){
+        return evenHtmlElement(content,"p");
+    }
+
+    public static String table(String content){
+        return evenHtmlElement(content,"table");
+    }
+
+    public static String tr(String content){
+        return evenHtmlElement(content,"tr");
+    }
+    public static String th(String content){
+        return evenHtmlElement(content,"th");
+    }
+    public static String td(String content){
+        return evenHtmlElement(content,"td");
+    }
+
+    private static String evenHtmlElement(String content, String elementName){
+        return "<"+elementName+">"+content+"</"+elementName+">";
+    }
+
+}
