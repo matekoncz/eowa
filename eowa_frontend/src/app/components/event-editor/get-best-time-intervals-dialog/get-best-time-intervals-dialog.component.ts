4fc05d5e585859003ec6af35bb58229f461a3c18
Koncz Máté <h265733@stud.u-szeged.hu>
2025-03-26 15:20:57 +0100

    create dialog to get parameters for getBestTimeIntervals

---
+++ eowa_frontend/src/app/components/event-editor/get-best-time-intervals-dialog/get-best-time-intervals-dialog.component.ts
@@ -0,0 +1,73 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { EowaEvent } from '../../../Model/EowaEvent';
+import { UserOpinion } from '../../../Model/Opinion';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-get-best-time-intervals-dialog',
+  standalone: true,
+  imports: [
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogTitle,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+  ],
+  templateUrl: './get-best-time-intervals-dialog.component.html',
+  styleUrl: './get-best-time-intervals-dialog.component.css',
+})
+export class GetBestTimeIntervalsDialogComponent {
+  event: EowaEvent = inject<EowaEvent>(MAT_DIALOG_DATA);
+
+  intervalsForm: FormGroup = new FormGroup({
+    participants: new FormControl(0, [
+      Validators.required,
+      Validators.min(0),
+      Validators.max(this.event.participants.length),
+    ]),
+    length: new FormControl(0, [
+      Validators.required,
+      Validators.min(0),
+      Validators.max(24 * 14),
+    ]),
+    allowedOpinions: new FormControl([], [Validators.required]),
+  });
+
+  UserOpinion = UserOpinion;
+
+  readonly dialogRef = inject(
+    MatDialogRef<GetBestTimeIntervalsDialogComponent>
+  );
+
+  onNoClick() {
+    this.dialogRef.close(null);
+  }
+  onOkClick() {
+    this.dialogRef.close({
+      participants: this.intervalsForm.get('participants')?.value,
+      length: this.intervalsForm.get('length')?.value,
+      allowedOpinions: this.intervalsForm.get('allowedOpinions')?.value,
+    });
+  }
+}
