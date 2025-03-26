88ca63d3f9a09f844d582c28fba0a6f6d0b8d218
Koncz Máté <h265733@stud.u-szeged.hu>
2025-03-26 15:16:20 +0100

    display unread mails

--- eowa_frontend/src/app/app.component.ts
+++ eowa_frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, Router, RouterModule} from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
@@ -9,6 +9,11 @@ import { MatDialog } from '@angular/material/dialog';
 import { LogoutDialogComponent } from './components/logout-dialog/logout-dialog.component';
 import { AuthService } from './services/auth.service';
 import {AuthStatus} from './services/auth.service';
+import { MailService } from './services/mail.service';
+import { from } from 'rxjs';
+import { Mail } from './Model/Mail';
+import { PopUpInfoComponent } from './components/popup-info/popup-info.component';
+import { EventService } from './services/event.service';
 
 @Component({
   selector: 'app-root',
@@ -17,12 +22,30 @@ import {AuthStatus} from './services/auth.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'eowa';
   dialog = inject(MatDialog);
   AuthStatus = AuthStatus;
 
-  constructor(private router: Router, private authservice: AuthService){}
+  constructor(private router: Router, private authservice: AuthService, private mailservice: MailService){}
+
+  ngOnInit(): void {
+    this.mailservice.getUnreadMails().subscribe((response) => {
+      let status = response.status;
+      if(status == 200){
+        from(response.json()).subscribe((mails: Mail[]) => {
+          mails.forEach((mail) => {
+            console.log(mail.content);
+             const dialogref = this.dialog.open(PopUpInfoComponent,{data:{title: mail.title, html: mail.content}});
+
+             dialogref.afterClosed().subscribe(() => {
+              this.mailservice.readMail(mail.id!)
+             })
+          });
+        });
+      }
+  });
+  }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(LogoutDialogComponent);
