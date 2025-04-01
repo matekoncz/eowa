022219c9178aedb0841d9e7ab74430c3e7a77791
Koncz Máté <h265733@stud.u-szeged.hu>
2025-04-01 14:00:36 +0200

    add authGuard

--- eowa_frontend/src/app/app.routes.ts
+++ eowa_frontend/src/app/app.routes.ts
@@ -7,14 +7,15 @@ import { MyEventsComponent } from './components/my-events/my-events.component';
 import { CreateCalendarComponent } from './components/create-calendar/create-calendar.component';
 import { ViewEventComponent } from './components/event-editor/view-event/view-event.component';
 import { ViewCalendarComponent } from './components/event-editor/view-calendar/view-calendar.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {path: 'signup', component: SignupComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'join-event', component: JoinEventComponent},
-    {path: 'create-event', component: CreateEventComponent},
-    {path: 'my-events', component: MyEventsComponent},
-    {path: 'create-calendar', component: CreateCalendarComponent},
-    {path: 'view-event', component: ViewEventComponent},
-    {path: 'view-calendar', component: ViewCalendarComponent}
+    {path: 'join-event', component: JoinEventComponent, canActivate: [authGuard]},
+    {path: 'create-event', component: CreateEventComponent, canActivate: [authGuard]},
+    {path: 'my-events', component: MyEventsComponent, canActivate: [authGuard]},
+    {path: 'create-calendar', component: CreateCalendarComponent, canActivate: [authGuard]},
+    {path: 'view-event', component: ViewEventComponent, canActivate: [authGuard]},
+    {path: 'view-calendar', component: ViewCalendarComponent, canActivate: [authGuard]}
 ];
